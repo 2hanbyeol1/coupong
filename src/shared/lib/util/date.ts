0e1462ea1e305d771945c0869d090e202ae6c162
2hanbyeol1 <2hanbyeol1@naver.com>
2025-09-15 15:19:10 +0900

    fix: 기간 만료 로직 수정, 시간 무시

--- src/shared/lib/util/date.ts
+++ src/shared/lib/util/date.ts
@@ -1,12 +1,20 @@
 /**
- * 날짜가 유효 기간을 초과했는지 체크
+ * 날짜가 유효 기간을 초과했는지 체크, 시간은 무시합니다.
  * @param date 날짜
  * @returns 유효 기간이 지났는지 여부
  */
 export const getExpired = (date: string) => {
   const today = new Date();
   const day = new Date(date);
-  return day < today;
+
+  const todayOnly = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+  );
+  const dayOnly = new Date(day.getFullYear(), day.getMonth(), day.getDate());
+
+  return dayOnly < todayOnly;
 };
 
 export const getYYYYMMDD = (date: string) => {
